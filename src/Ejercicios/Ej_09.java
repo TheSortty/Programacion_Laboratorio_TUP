742a258f3ad5ae4094a3d66351d7cf93a161e563
TheSortty <102250795+TheSortty@users.noreply.github.com>
2023-08-16 11:08:32 -0300

    Arreglo de Ej_09, tengo confucion si deberia imprimir los numeros del 1 al 100, divisibles entre 2 como 3 o 2 y 3 de manera individual...

--- src/Ejercicios/Ej_09.java
+++ src/Ejercicios/Ej_09.java
@@ -2,10 +2,12 @@ package Ejercicios;
 
 public class Ej_09 {
     public static void main(String[] args) {
-        for (int i = 0; i <= 100; i++){
-            if (i % 2 == 0){
+        for (int i = 1; i <= 100; i++){
+            if (i==1){
                 System.out.println(i);
-            }else if(i % 3 == 0){
+            }else if (i % 2 == 0 && i % 3 == 0){
+                System.out.println(i);
+            }else if (i==100){
                 System.out.println(i);
             }
         }
